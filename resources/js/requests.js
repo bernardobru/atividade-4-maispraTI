3a40ae0414b41aa0ecc033300436040b5a0db33d
bernardobru <tutibruxel@gmail.com>
2024-07-26 13:40:56 -0300

    added async requests with XMLHttprequest()

--- resources/js/requests.js
+++ resources/js/requests.js
@@ -1,3 +1,26 @@
-function () {
-    
+function request(url) {
+    document.getElementById('content').innerHTML = '';
+    if(!document.getElementById('loading')) {
+        const imgLoading = document.createElement('img');
+        imgLoading.id = 'loading';
+        imgLoading.src = '../slick/ajax-loader.gif';
+        imgLoading.className = 'rounded mx-auto d-block';
+        document.getElementById('content').appendChild(imgLoading);
+    }
+    const ajax = new XMLHttpRequest();
+    ajax.open('GET', url)
+    ajax.onreadystatechange = () => {
+        if(ajax.readyState === 4 && ajax.status === 200) {
+            document.getElementById('content').innerHTML = ajax.responseText;
+        }
+        if(ajax.readyState == 4 && ajax.status == 404) {
+            document.getElementById('content').innerHTML = 'Erro 404';
+        }   
+    }
+    ajax.send();
+}
+function request2(url) {
+    document.getElementById('content').innerHTML = '';
+    const content = fetch(url)
+    .then((response))
 }
